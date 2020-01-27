b6fff3fb4efc59795071e3056e5d4ae2e554e3b7
Alexander Elchlepp <alex.pensionsverwaltung@gmail.com>
2020-01-27 19:19:15 +0100

    #11 miscellaneous positions will be prefilled on new invoices

--- src/Service/InvoiceService.php
+++ src/Service/InvoiceService.php
@@ -283,55 +283,80 @@ class InvoiceService implements ITemplateRenderer
     }
     
     /**
-     * Retrieves valid prices for each day of stay and prefills the appartment position for the reservation
+     * Retrieves valid prices for each day of stay and prefills the apartment position for the reservation
+     * each day has exactly one valid price category
      * @param Reservation $reservation
      * @param SessionInterface $session
      */
     public function prefillAppartmentPositions(Reservation $reservation, SessionInterface $session) {
         $prices = $this->ps->getPrices($reservation, 2);
-        $this->prefillPositions($reservation, $prices, $session, 2);
-    }
-    
-    public function prefillMiscPositions(Reservation $reservation, SessionInterface $session) {
-        $prices = $this->ps->getPrices($reservation, 1);
-        $this->prefillPositions($reservation, $prices, $session, 1);
-    }
-    
-    private function prefillPositions(Reservation $reservation, array $prices, SessionInterface $session, int $type) {
         $days = $this->getDateDiff($reservation->getStartDate(), $reservation->getEndDate());
         
         $curDate = clone $reservation->getStartDate();
-        $lastPrice = $prices[0];
-        $start = clone $reservation->getStartDate();        
-        $i = 0;
-        // loop through each day and create the position based on the retrieved price for this day
-        do {      
+        $lastPrice = ($prices[0] === null ? null : $prices[0][0]);
+        $start = clone $reservation->getStartDate();   
+        
+        for($i = 0; $i <= $days; $i++) {
+            // here we need to ignore the price of the last day because it's not the valid price e.g. booked from 01.01 - 02.01 we need to use the price for 01.01.
+            // thats why we apply the prevois price for the last loop
             if($i < $days) {
-                $price = $prices[$i];
+                $price = ($prices[$i] === null ? null : $prices[$i][0]);
             } else {
                 $price = $lastPrice;
-            }
-            
+            }                
+
             $curDate = (clone $curDate)->add(new \DateInterval("P".($i === 0 ? 0 : 1)."D"));
-            if($price !== null && $lastPrice !== null && ($lastPrice->getId() !== $price->getId() || $i == $days)) {
-                if($type === 1) {
-                    $position = $this->makeMiscPosition($start, $curDate, $reservation, $lastPrice);
-                    $this->saveNewMiscPosition($position, $session);
-                } else {
-                    $position = $this->makeAparmtentPosition($start, $curDate, $reservation, $lastPrice);
-                    $this->saveNewAppartmentPosition($position, $session);
-                }
-                
+            if($price !== null && $lastPrice !== null && ($lastPrice->getId() !== $price->getId() || $i == $days)) {                
+                $position = $this->makeAparmtentPosition($start, $curDate, $reservation, $lastPrice);
+                $this->saveNewAppartmentPosition($position, $session);
+
                 $start = clone $curDate;
             }
-            
             $lastPrice = $price;
-            $i++;
-        } while($i <= $days);   // loop must run one more time to add the position for the last day of stay
+        }    // loop must run one more time to add the position for the last day of stay
     }
     
     /**
-     * Stores a new appartment position in the session
+     * Retrieves valid prices for each day of stay and prefills the miscellaneous position for the reservation
+     * each day can have more than one active price category
+     * @param Reservation $reservation
+     * @param SessionInterface $session
+     */
+    public function prefillMiscPositions(array $reservationIds, SessionInterface $session) {
+        $tmpMiscArr = [];
+        // loop over all selected reservations, this avoids dublicate entries in the result, prices that are equal will be aggregated
+        foreach($reservationIds as $resId) {
+            $reservation = $this->em->getRepository(Reservation::class)->find($resId);                
+             
+            $prices = $this->ps->getPrices($reservation, 1);  
+
+            $days = $this->getDateDiff($reservation->getStartDate(), $reservation->getEndDate());
+
+            // loop through each day and create the position based on the retrieved prices for this day
+            for($i = 1; $i <= $days; $i++) {
+                if($prices[$i] === null) {
+                    continue;
+                }
+                foreach($prices[$i] as $price) {  
+                    
+                    // if key exists, add the current amount to the existing one, to have only one entry in the results list 
+                    // with the same price id but a total amount if the same price category occurs more than once
+                    if(array_key_exists($price->getId(), $tmpMiscArr)) {
+                        $tmpMiscArr[$price->getId()]['amount'] +=  1 * $reservation->getPersons();
+                    } else {
+                        $tmpMiscArr[$price->getId()] = [
+                            'price' => $price,
+                            'amount' => 1
+                        ];
+                    }
+                }
+            }
+        }
+        $this->makeMiscPositions($reservation, $tmpMiscArr, $session);
+    }
+    
+    /**
+     * Stores a new apartment position in the session
      * @param InvoiceAppartment $position
      * @param SessionInterface $session
      */
@@ -342,6 +367,11 @@ class InvoiceService implements ITemplateRenderer
         $session->set("invoicePositionsAppartments", $newInvoicePositionsAppartmentsArray);
     }
     
+    /**
+     * Stores a new miscellaneous position in the session
+     * @param InvoicePosition $position
+     * @param SessionInterface $session
+     */
     public function saveNewMiscPosition(InvoicePosition $position, SessionInterface $session) {
         $newInvoicePositionsMiscArray = $session->get("invoicePositionsMiscellaneous");                
         $newInvoicePositionsMiscArray[] = $position;
@@ -371,12 +401,33 @@ class InvoiceService implements ITemplateRenderer
         return $positionAppartment;
     }
     
-     private function makeMiscPosition(\DateTime $start, \DateTime $end, Reservation $reservation, Price $price) : InvoiceAppartment {
-        $position = new InvoicePosition();
-                
-        return $position;
+    /**
+     * 
+     * @param Reservation $reservation
+     * @param array $tmpPricesArr
+     * @return InvoicePosition[]
+     */
+     private function makeMiscPositions(Reservation $reservation, array $tmpPricesArr, SessionInterface $session) : array {
+        $positions = [];
+         foreach($tmpPricesArr as $tmpPrice) {
+            $position = new InvoicePosition();
+            $position->setAmount( $tmpPrice['amount'] );
+            $position->setDescription( $tmpPrice['price']->getDescription() );
+            $position->setPrice( $tmpPrice['price']->getPrice() );
+            $position->setVat( $tmpPrice['price']->getVat() );
+            
+            $this->saveNewMiscPosition($position, $session);
+        }
+                         
+        return $positions;
     }
     
+    /**
+     * Helper function to get number of days between two dates
+     * @param \DateTime $start
+     * @param \DateTime $end
+     * @return int
+     */
     private function getDateDiff(\DateTime $start, \DateTime $end) : int {
         $interval = date_diff($start, $end);
 		
