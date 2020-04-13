e85e3bf7cfcccf6853983fe4856cebb275fda0b3
Alexander Elchlepp <alex.pensionsverwaltung@gmail.com>
2020-04-13 15:37:56 +0200

    changed mail address

--- src/Controller/ReservationOriginServiceController.php
+++ src/Controller/ReservationOriginServiceController.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the guesthouse administration package.
  *
- * (c) Alexander Elchlepp <alex.pensionsverwaltung@gmail.com>
+ * (c) Alexander Elchlepp <info@fewohbee.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
