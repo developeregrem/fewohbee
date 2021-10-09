c5b0074c6fcb93ca8ef3312ba30061164e253aae
Alexander Elchlepp <alex.pensionsverwaltung@gmail.com>
2021-10-09 12:07:26 +0200

    added more information to the room column in the reservation table

--- src/Form/RoomCategoryType.php
+++ src/Form/RoomCategoryType.php
@@ -6,6 +6,7 @@ use App\Entity\RoomCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RoomCategoryType extends AbstractType
 {
@@ -13,6 +14,7 @@ class RoomCategoryType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('acronym', TextType::class, ['label' => 'category.acronym'])
         ;
     }
 
