fb751d5f06a26c5d77aa1a4192ac5d2c369692de
Alexander Elchlepp <alex.pensionsverwaltung@gmail.com>
2022-05-24 15:54:14 +0200

    #49 added copy to clipboard for ical address

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -1,21 +1,21 @@
-jQuery.fn.delayKeyup = function( cb, delay ){
-    if(delay === null){
-      delay = 400;
+jQuery.fn.delayKeyup = function (cb, delay) {
+    if (delay === null) {
+        delay = 400;
     }
     var timer = 0;
-    return $(this).on('keyup',function(){
-      clearTimeout(timer);
-      timer = setTimeout( cb , delay );
+    return $(this).on('keyup', function () {
+        clearTimeout(timer);
+        timer = setTimeout(cb, delay);
     });
 };
 
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
 }
 
-Date.prototype.minusDays = function(days) {
+Date.prototype.minusDays = function (days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() - days);
     return date;
@@ -30,15 +30,15 @@ Date.prototype.minusDays = function(days) {
 function iniStartOrEndDate(idStart, idEnd, addDays) {
     let end = document.getElementById(idEnd);
     let start = document.getElementById(idStart);
-    if(end.value === '' && start.value !== '') {
+    if (end.value === '' && start.value !== '') {
         let dEnd = new Date(start.value).addDays(addDays);
-        let sDate = dEnd.getFullYear()+'-'+('0' + (dEnd.getMonth()+1)).slice(-2)+'-'+('0' + dEnd.getDate()).slice(-2);
+        let sDate = dEnd.getFullYear() + '-' + ('0' + (dEnd.getMonth() + 1)).slice(-2) + '-' + ('0' + dEnd.getDate()).slice(-2);
         end.value = sDate;
     }
 
-    if(start.value === '' && end.value !== '') {
-       let dStart = new Date(end.value).minusDays(addDays);
-        let sDate = dStart.getFullYear()+'-'+('0' + (dStart.getMonth()+1)).slice(-2)+'-'+('0' + dStart.getDate()).slice(-2);
+    if (start.value === '' && end.value !== '') {
+        let dStart = new Date(end.value).minusDays(addDays);
+        let sDate = dStart.getFullYear() + '-' + ('0' + (dStart.getMonth() + 1)).slice(-2) + '-' + ('0' + dStart.getDate()).slice(-2);
         start.value = sDate;
     }
 }
@@ -51,10 +51,43 @@ function iniStartOrEndDate(idStart, idEnd, addDays) {
  * @param function handler a function that shall be executed once the event is fired
  * @returns void
  */
- const delegate = (element, type, selector, handler) => {
-        element.addEventListener(type, (event) => {
-          if (event.target.closest(selector)) {
+const delegate = (element, type, selector, handler) => {
+    element.addEventListener(type, (event) => {
+        if (event.target.closest(selector)) {
             handler(event);
-          }
-        });
-    };
+        }
+    });
+};
+
+/**
+ * Copy the text of a near textfield to clipboard
+ * @param {Element} elm
+ * @returns {void}
+ */
+function copyToClipboard(elm) {
+    if (elm) {
+        let target = elm.closest('div').querySelector('input[type=text]');
+        if (target) {
+            target.select();
+            target.setSelectionRange(0, target.value.lengt);
+            let suceed;
+            try {
+                suceed = navigator.clipboard.writeText(target.value);
+            } catch (e) {
+                console.warn(e);
+                suceed = false;
+            }
+
+            if (suceed) {
+                var popover = new bootstrap.Popover(target, {
+                    'content': elm.dataset.hint,
+                    'placement': 'top',
+                });
+                popover.show();
+                setTimeout(function () {
+                    popover.dispose();
+                }, 1500);
+            }
+        }
+    }
+}
