2be4ae193704f3a6f1ca4dc91a9bd5fac03b45cc
Alexander Elchlepp <alexander.elchlepp@gmail.com>
2025-11-26 16:49:17 +0100

    #112 better error handling on login failure

---
+++ assets/controllers/login_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+
+    connect() {
+        const hasNav = document.querySelector('body nav') !== null;
+
+        if (hasNav && this.hasStartUrlValue && this.startUrlValue) {
+            window.location.href = this.startUrlValue;
+            return;
+        }
+
+        document.querySelectorAll('body div.d-none, body p.d-none').forEach((element) => {
+            element.classList.remove('d-none');
+        });
+    }
+
+}
