ee733417f01050bedeeb74df9bb39cf410fd413b
Alexander Elchlepp <alexander.elchlepp@gmail.com>
2025-11-26 16:49:17 +0100

    #112 better error handling on login failure

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import LoginController from './controllers/login_controller.js';
 
 const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+app.register('login', LoginController);
