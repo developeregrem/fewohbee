4e0cfa2cd3a76b3fb4c63fefa922777b0e6b5b3a
Alexander Elchlepp <alexander.elchlepp@gmail.com>
2025-11-29 14:39:30 +0100

    #112 user can edit profile information

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import LoginController from './controllers/login_controller.js';
+import ProfileFormController from './controllers/profile_form_controller.js';
 
 const app = startStimulusApp();
 app.register('login', LoginController);
+app.register('profile-form', ProfileFormController);
