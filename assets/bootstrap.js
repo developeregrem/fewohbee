1848d21e1c8956c871719ea53cf33073ea056415
Alexander Elchlepp <alexander.elchlepp@gmail.com>
2025-12-01 11:54:16 +0100

    refactoring copy-to-clipboard

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,9 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import LoginController from './controllers/login_controller.js';
 import ProfileFormController from './controllers/profile_form_controller.js';
+import CopyController from './controllers/copy_controller.js';
 
 const app = startStimulusApp();
 app.register('login', LoginController);
 app.register('profile-form', ProfileFormController);
+app.register('copy', CopyController);
