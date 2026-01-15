f8598702fad2751d421fe324d91b106b71888ce3
Alexander Elchlepp <alexander.elchlepp@gmail.com>
2026-01-15 10:24:04 +0100

    renamed migration

--- migrations/Version20260116120000.php
+++ migrations/Version20260116120000.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260416120000 extends AbstractMigration
+final class Version20260116120000 extends AbstractMigration
 {
     public function getDescription(): string
     {
